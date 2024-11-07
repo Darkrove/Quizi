a238212b46eff560ab62cd95f571efa4db07fcc7
Shaikh <ext-sajjadabdul.shaikh@here.com>
2024-11-07 09:49:05 +0530

    updated route

--- src/app/api/save-quiz-result/route.ts
+++ src/app/api/save-quiz-result/route.ts
@@ -3,34 +3,36 @@ import { prisma } from '@/lib/prisma';
 import messages from '@/constants/messages';
 import { z } from "zod"
 
+const quizSchema = z.object({
+  name: z.string(),
+  employeeId: z.string(),
+  score: z.number(),
+  totalQuestions: z.number(),
+  timestamp: z.string().transform((val) => new Date(val)),
+  answers: z.array(
+    z.object({
+      questionId: z.string(),
+      questionText: z.string(),
+      selectedAnswer: z.string(),
+    })
+  ),
+});
+
 export async function POST(request: NextRequest) {
   try {
-    const { name, employeeId, score, totalQuestions, timestamp, answers } = await request.json();
+    const data = quizSchema.parse(await request.json());
 
     const result = await prisma.quizResult.create({
       data: {
-        name,
-        employeeId,
-        score,
-        totalQuestions,
-        timestamp: new Date(timestamp),
-        answers: answers.map((answer: { questionId: string; questionText: string; selectedAnswer: string }) => ({
-          questionId: answer.questionId,
-          questionText: answer.questionText,
-          selectedAnswer: answer.selectedAnswer,
-        })),
+        ...data
       },
     });
 
     return NextResponse.json({ success: true, insertedId: result.id });
-  }  catch (error) {
+  } catch (error) {
     if (error instanceof z.ZodError) {
-      console.log(error)
-      return NextResponse.json({ error: error.issues }, { status: 400 })
+      return NextResponse.json({ error: error.issues }, { status: 400 });
     }
-    return NextResponse.json(
-      { error, message: messages.request.failed },
-      { status: 500 }
-    )
+    return NextResponse.json({ error, message: messages.request.failed }, { status: 500 });
   }
 }
